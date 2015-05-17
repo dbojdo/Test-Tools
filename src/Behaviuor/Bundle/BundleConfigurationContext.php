f49bee30c10fb628c8cfb7a7b38b42f8d5e41b87
Daniel Bojdo <daniel@bojdo.eu>
2015-05-17 09:37:19 +0100

    [new] standalone bundle testing

---
+++ src/Behaviuor/Bundle/BundleConfigurationContext.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * File BundleConfigurationContext.php
+ * Created at: 2015-05-17 09-22
+ *
+ * @author Daniel Bojdo <daniel.bojdo@web-it.eu>
+ */
+
+namespace Webit\Tests\Behaviour\Bundle;
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+abstract class BundleConfigurationContext implements Context
+{
+    /**
+     * @var Kernel
+     */
+    protected $kernel;
+
+    /**
+     * @var array
+     */
+    protected $services = array();
+
+    /**
+     * @param Kernel $kernel
+     */
+    public function __construct(Kernel $kernel)
+    {
+        $this->kernel = $this->kernel;
+    }
+
+    /**
+     * @Given the configuration contains:
+     * @param PyStringNode $string
+     */
+    public function theConfigurationContains(PyStringNode $string)
+    {
+        $this->kernel->appendConfig($string->getRaw());
+    }
+
+    /**
+     * @When I bootstrap the application
+     */
+    public function iBootstrapTheApplication()
+    {
+        $this->kernel->boot();
+    }
+
+    /**
+     * @Then there should be following services defined:
+     * @param PyStringNode $string
+     */
+    public function thereShouldBeFollowingServicesDefined(PyStringNode $string)
+    {
+        $container = $this->kernel->getRawContainerBuilder();
+        foreach (explode(',',$string->getRaw()) as $serviceName) {
+            $serviceName = trim($serviceName);
+            if (empty($serviceName)) {continue;}
+
+            \PHPUnit_Framework_Assert::assertTrue($container->hasDefinition($serviceName), sprintf('Service "%s" is not defined in the Service Container.', $serviceName));
+            $this->services[] = $serviceName;
+        }
+    }
+
+    /**
+     * @Then there should be following aliases defined:
+     * @param TableNode $table
+     */
+    public function thereShouldBeFollowingAliasesDefined(TableNode $table)
+    {
+        $container = $this->kernel->getRawContainerBuilder();
+        foreach ($table as $row) {
+            \PHPUnit_Framework_Assert::assertTrue($container->hasAlias($row['alias']),
+                sprintf('Alias "%s" doesn\'t exist', $row['alias']));
+            \PHPUnit_Framework_Assert::assertEquals($row['service'], (string)$container->getAlias($row['alias']));
+            $this->services[] = $row['alias'];
+        }
+    }
+
+    /**
+     * @Then there should not be following services defined:
+     * @param PyStringNode $string
+     */
+    public function thereShouldNotBeFollowingServicesDefined(PyStringNode $string)
+    {
+        $container = $this->kernel->getRawContainerBuilder();
+        foreach (explode(',',$string->getRaw()) as $serviceName) {
+            $serviceName = trim($serviceName);
+            if (empty($serviceName)) {continue;}
+            \PHPUnit_Framework_Assert::assertFalse($container->hasDefinition($serviceName), sprintf('Service "%s" is defined in the Service Container but it should not be.', $serviceName));
+        }
+    }
+
+    /**
+     * @Then all given services should be reachable
+     */
+    public function allGivenServicesShouldBeReachable()
+    {
+        foreach ($this->services as $serviceName) {
+            $this->kernel->getContainer()->get($serviceName);
+        }
+    }
+}
---
+++ src/Behaviuor/Bundle/Kernel.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * File Kernel.php
+ * Created at: 2015-05-17 09-22
+ *
+ * @author Daniel Bojdo <daniel.bojdo@web-it.eu>
+ */
+
+namespace Webit\Tests\Behaviour\Bundle;
+
+use Symfony\Component\HttpKernel\HttpKernel;
+
+abstract class Kernel extends HttpKernel
+{
+    /**
+     * @var string
+     */
+    private $hash;
+
+    /**
+     * @var ContainerBuilder
+     */
+    private $containerBuilder;
+
+    /**
+     * @var array
+     */
+    private $configs = array();
+
+    public function __construct($env = 'test', $debug = true)
+    {
+        parent::__construct($env, $debug);
+        $this->hash = substr(md5(mt_rand(0,10000).microtime(true). mt_rand(0, 100000)), -6);
+    }
+
+    /**
+     * @param $yaml
+     */
+    public function appendConfig($yaml) {
+        $this->configs[] = $yaml;
+    }
+
+    /**
+     * Loads the container configuration.
+     *
+     * @param LoaderInterface $loader A LoaderInterface instance
+     *
+     * @api
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $config = $this->getRootDir(). '/config.yml';
+        if (is_file($config)) {
+            $loader->load($config);
+        }
+
+        foreach ($this->configs as $i => $config) {
+            $file = $this->getCacheDir() .'/config'.$i.'.yml';
+            file_put_contents($file, $config);
+            $loader->load($file);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return sys_get_temp_dir() . '/kernel/'.$this->hash.'/cache';
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return sys_get_temp_dir() . '/kernel/'.$this->hash.'/logs';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getContainerClass()
+    {
+        $class = parent::getContainerClass();
+        return $class . ucfirst($this->hash);
+    }
+
+    /**
+     * @return ContainerBuilder
+     */
+    public function getRawContainerBuilder()
+    {
+        if (! $this->containerBuilder) {
+            $this->containerBuilder = $this->buildContainer();
+            $this->containerBuilder->compile();
+        }
+
+        return $this->containerBuilder;
+    }
+}
